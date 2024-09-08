19df7cab9e53971dbe23bca98d412ea357392de6
nainglinnkhant <nainglinnkhnt@gmail.com>
2024-09-08 13:38:08 +0700

    chore: add utility types

--- src/utilityTypes.ts
+++ src/utilityTypes.ts
@@ -1,22 +1,22 @@
-import { Post } from '../types'
+import { Post } from "../types"
 
 // Parameters
 type FuncType = (name: string, age: number) => void
 const logArgs: FuncType = (name, age) => {
   console.log(name, age)
 }
-const arr: Parameters<FuncType> = ['John', 20]
+const arr: Parameters<FuncType> = ["John", 20]
 
 // Awaited is used to get resolved values of Promise<>
 const getPosts = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts")
   const posts: Awaited<Promise<Post[]>> = await res.json()
 }
 
 // We can extract one specific type of a union type using this syntax
 type Color = Extract<
-  { type: 'rgb'; value: string } | { type: 'hsl'; value: string },
-  { type: 'rgb' }
+  { type: "rgb"; value: string } | { type: "hsl"; value: string },
+  { type: "rgb" }
 >
 
 // Deep Partial
@@ -44,9 +44,9 @@ interface SocialPost {
 }
 // In the case below, we don't need to specify the 'description' prop anymore whereas we have to if we used the original Partial
 const socialPost: DeepPartial<SocialPost> = {
-  id: '12',
+  id: "12",
   meta: {
-    name: 'Foo',
+    name: "Foo",
   },
 }
 
@@ -54,3 +54,14 @@ const socialPost: DeepPartial<SocialPost> = {
 type Prettify<T> = {
   [K in keyof T]: T[K]
 } & {}
+
+type Spread<T1, T2> = T2 & Omit<T1, keyof T2>
+
+type StrictOmit<T, K extends keyof T> = Omit<T, K>
+
+type AnyStringExceptError<T> = T extends "error" ? never : T
+
+function logString<T extends string>(str: AnyStringExceptError<T>) {
+  console.log(str)
+}
+// logString('error')
